ff06fac4ce33b0b0f95780faf492fee31cc9eede
Matthew Xu <matthewliuxu2005@gmail.com>
2026-04-28 16:29:34 -0500

    Create constructor

--- src/main/java/domain/piece/Piece.java
+++ src/main/java/domain/piece/Piece.java
@@ -2,4 +2,15 @@ package domain.piece;
 
 public abstract class Piece {
 
+    private final PieceType type;
+    private final PieceColor color;
+    protected final boolean canJump;
+    private boolean hasMoved;
+
+    public Piece(PieceType type, PieceColor color) {
+        this.type = type;
+        this.color = color;
+        this.canJump = (PieceType.KNIGHT == type);
+        this.hasMoved = false;
+    }
 }
