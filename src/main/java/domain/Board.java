e6fef2abfbb196fe312ab28775ac611d77d9ff73
Matthew Xu <matthewliuxu2005@gmail.com>
2026-05-03 17:07:44 -0500

    create Board

--- src/main/java/domain/Board.java
+++ src/main/java/domain/Board.java
@@ -1,17 +1,4 @@
 package domain;
 
-import domain.gamestate.GameState;
-import domain.piece.Piece;
-
-/**
- * Board model contract (implemented by the Board feature branch). {@link ui.BoardController}
- * depends on this interface; unit tests substitute an EasyMock proxy.
- */
-public interface Board {
-
-  Piece[][] getSnapshot();
-
-  GameState getCurrentGameState();
-
-  Piece getPieceAt(int rank, int file);
+public class Board {
 }
