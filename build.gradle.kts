6cb7c35612f8a2e7364a314efc5e0a3091d5cf32
didiermun <didiermunezer38@gmail.com>
2026-05-11 14:20:55 -0500

    chore: set up checkstyle and spotbugs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import org.gradle.api.plugins.quality.Checkstyle
+
 plugins {
     id("java")
+    checkstyle
+    id("com.github.spotbugs") version "6.0.26"
 }
 
 group = "nu.csse.sqe"
@@ -7,6 +11,22 @@ version = "1.0"
 
 repositories {
     mavenCentral()
+}
+
+checkstyle {
+    toolVersion = "10.21.4"
+    configFile = file("config/checkstyle/checkstyle.xml")
+}
+
+tasks.withType<Checkstyle>().configureEach {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+tasks.named("check") {
+    dependsOn("spotbugsMain", "spotbugsTest")
 }
 
 dependencies {
