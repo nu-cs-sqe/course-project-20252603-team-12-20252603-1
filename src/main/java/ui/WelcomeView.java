d1433194dc7bc45a0d8fe168a2185eeb291c4081
Alex-Anca <alexanca2027@u.northwestern.edu>
2026-05-25 16:53:57 -0500

    Add WelcomeView class skeleton

---
+++ src/main/java/ui/WelcomeView.java
@@ -0,0 +1,31 @@
+package ui;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class WelcomeView extends JFrame {
+
+    private JTextField player1NameField;
+    private JTextField player2NameField;
+
+    public WelcomeView() {
+    }
+
+    public String getPlayer1Name() {
+        return "";
+    }
+
+    public String getPlayer2Name() {
+        return "";
+    }
+
+    void setPlayer1Name(String name) {
+    }
+
+    void setPlayer2Name(String name) {
+    }
+
+    private void createWelcomeScreenUI() {
+        // untestable: Swing UI assembly
+    }
+}
