3d58f454f87001e8aadcb775c1cf8c9c10609504
Matthew Xu <matthewliuxu2005@gmail.com>
2026-06-04 20:09:13 -0500

    implement setPlayAgainAction

--- src/main/java/ui/EndGameView.java
+++ src/main/java/ui/EndGameView.java
@@ -4,8 +4,14 @@ import javax.swing.JFrame;
 
 class EndGameView extends JFrame {
 
+    private Runnable playAgainAction = () -> {};
+
     EndGameView(String resultMessage) {
         buildUi(resultMessage);
+    }
+
+    void setPlayAgainAction(Runnable action) {
+        this.playAgainAction = action;
     }
 
     private void buildUi(String resultMessage) {
