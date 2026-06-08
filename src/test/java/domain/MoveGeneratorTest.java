75829ed23346a8bf4ba393c15c840171ab865d0c
didiermun <didiermunezer38@gmail.com>
2026-06-07 23:15:44 -0500

    IsInCheck_WhenPawnOnExactAttackSquare_ReturnsTrue passes

--- src/test/java/domain/MoveGeneratorTest.java
+++ src/test/java/domain/MoveGeneratorTest.java
@@ -1044,6 +1044,19 @@ class MoveGeneratorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void IsInCheck_WhenPawnOnExactAttackSquare_ReturnsTrue() {
+        Piece[][] board = emptyBoard();
+        board[3][4] = new King(PieceColor.WHITE);
+        board[2][3] = new Pawn(PieceColor.BLACK);
+        MoveGenerator moveGenerator = new MoveGenerator(board, Optional.empty());
+
+        boolean expected = true;
+        boolean actual = moveGenerator.isInCheck(PieceColor.WHITE);
+
+        assertEquals(expected, actual);
+    }
+
     private static boolean hasMoveToWithType(
             java.util.List<Move> moves, int file, int rank, MoveType type) {
         for (Move move : moves) {
