0bd59431c79169713195dae5cba0e4e49b269b9a
didiermun <didiermunezer38@gmail.com>
2026-06-07 23:15:36 -0500

    GenerateLegalMoves_OnKingAtCenter_IncludesSouthWestDestination passes

--- src/test/java/domain/MoveGeneratorTest.java
+++ src/test/java/domain/MoveGeneratorTest.java
@@ -1018,6 +1018,19 @@ class MoveGeneratorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void GenerateLegalMoves_OnKingAtCenter_IncludesSouthWestDestination() {
+        Piece[][] board = emptyBoard();
+        board[4][4] = new King(PieceColor.WHITE);
+        MoveGenerator moveGenerator = new MoveGenerator(board, Optional.empty());
+
+        boolean expected = true;
+        boolean actual = hasMoveTo(
+                moveGenerator.generateLegalMoves(new Location(4, 4)), 3, 5);
+
+        assertEquals(expected, actual);
+    }
+
     private static boolean hasMoveToWithType(
             java.util.List<Move> moves, int file, int rank, MoveType type) {
         for (Move move : moves) {
