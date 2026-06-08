b5cc92e25aa98cb360ba084e0f98826cbfaf038e
didiermun <didiermunezer38@gmail.com>
2026-06-07 23:14:27 -0500

    GenerateLegalMoves_OnWhitePawnAtStartWithBlockerAtTwoAhead_ExcludesTwoStep passes

--- src/test/java/domain/MoveGeneratorTest.java
+++ src/test/java/domain/MoveGeneratorTest.java
@@ -827,6 +827,20 @@ class MoveGeneratorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void GenerateLegalMoves_OnWhitePawnAtStartWithBlockerAtTwoAhead_ExcludesTwoStep() {
+        Piece[][] board = emptyBoard();
+        board[6][4] = new Pawn(PieceColor.WHITE);
+        board[4][4] = new Rook(PieceColor.BLACK);
+        MoveGenerator moveGenerator = new MoveGenerator(board, Optional.empty());
+
+        boolean expected = false;
+        boolean actual = hasMoveTo(
+                moveGenerator.generateLegalMoves(new Location(4, 6)), 4, 4);
+
+        assertEquals(expected, actual);
+    }
+
     private static boolean hasMoveToWithType(
             java.util.List<Move> moves, int file, int rank, MoveType type) {
         for (Move move : moves) {
