d2a2fa85c6c74d763de3eaa1ab162f3ace6dec71
didiermun <didiermunezer38@gmail.com>
2026-06-07 23:15:10 -0500

    GenerateLegalMoves_OnRookFacingEnemy_IncludesCaptureDestination passes

--- src/test/java/domain/MoveGeneratorTest.java
+++ src/test/java/domain/MoveGeneratorTest.java
@@ -952,6 +952,20 @@ class MoveGeneratorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void GenerateLegalMoves_OnRookFacingEnemy_IncludesCaptureDestination() {
+        Piece[][] board = emptyBoard();
+        board[4][4] = new Rook(PieceColor.WHITE);
+        board[4][6] = new Rook(PieceColor.BLACK);
+        MoveGenerator moveGenerator = new MoveGenerator(board, Optional.empty());
+
+        boolean expected = true;
+        boolean actual = hasMoveTo(
+                moveGenerator.generateLegalMoves(new Location(4, 4)), 4, 6);
+
+        assertEquals(expected, actual);
+    }
+
     private static boolean hasMoveToWithType(
             java.util.List<Move> moves, int file, int rank, MoveType type) {
         for (Move move : moves) {
