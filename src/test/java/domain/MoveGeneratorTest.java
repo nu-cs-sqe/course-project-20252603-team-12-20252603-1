7324cc112b868b1626a9107854f20c8511d84e07
didiermun <didiermunezer38@gmail.com>
2026-06-07 23:16:18 -0500

    GenerateLegalMoves_OnWhitePawnWithLeftDiagonalEnemy_ExcludesRightCapture passes

--- src/test/java/domain/MoveGeneratorTest.java
+++ src/test/java/domain/MoveGeneratorTest.java
@@ -1122,6 +1122,20 @@ class MoveGeneratorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void GenerateLegalMoves_OnWhitePawnWithLeftDiagonalEnemy_ExcludesRightCapture() {
+        Piece[][] board = emptyBoard();
+        board[4][4] = new Pawn(PieceColor.WHITE);
+        board[3][3] = new Rook(PieceColor.BLACK);
+        MoveGenerator moveGenerator = new MoveGenerator(board, Optional.empty());
+
+        boolean expected = false;
+        boolean actual = hasMoveTo(
+                moveGenerator.generateLegalMoves(new Location(4, 4)), 5, 3);
+
+        assertEquals(expected, actual);
+    }
+
     private static boolean hasMoveToWithType(
             java.util.List<Move> moves, int file, int rank, MoveType type) {
         for (Move move : moves) {
