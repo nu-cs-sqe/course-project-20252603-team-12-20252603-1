86bd2588fc88efdaa0158d8f383eefb748a61447
didiermun <didiermunezer38@gmail.com>
2026-06-07 23:14:36 -0500

    ApplyMoveToBoard_OnPromotionRook_DestinationHasRook passes

--- src/test/java/domain/MoveGeneratorTest.java
+++ src/test/java/domain/MoveGeneratorTest.java
@@ -854,6 +854,20 @@ class MoveGeneratorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void ApplyMoveToBoard_OnPromotionRook_DestinationHasRook() {
+        Piece[][] board = emptyBoard();
+        board[1][4] = new Pawn(PieceColor.WHITE);
+        Move move = new Move(
+                new Location(4, 1), new Location(4, 0), MoveType.PROMOTION, PieceType.ROOK);
+
+        Piece[][] result = MoveGenerator.applyMoveToBoard(board, move);
+
+        PieceType expected = PieceType.ROOK;
+        PieceType actual = result[0][4].getType();
+        assertEquals(expected, actual);
+    }
+
     private static boolean hasMoveToWithType(
             java.util.List<Move> moves, int file, int rank, MoveType type) {
         for (Move move : moves) {
