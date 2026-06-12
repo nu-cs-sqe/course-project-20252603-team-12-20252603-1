b8238b4fce3ea0af474d019447200f9ebeb5a117
Matthew Xu <matthewliuxu2005@gmail.com>
2026-06-12 00:01:44 -0500

    shorten message to fit on 1 line

--- src/main/java/domain/MoveGenerator.java
+++ src/main/java/domain/MoveGenerator.java
@@ -152,8 +152,7 @@ public class MoveGenerator {
         if (move.getType() == MoveType.CASTLING_QUEENSIDE) {
             PieceColor color = copy[fromRank][fromFile].getColor();
             int rookFile = findUnmovedRookFileIn(copy, fromRank, fromFile, false, color)
-                    .orElseThrow(() ->
-                            new IllegalStateException("No unmoved queenside rook found"));
+                    .orElseThrow(() -> new IllegalStateException("No queenside rook found"));
             Piece king = copy[fromRank][fromFile];
             final Piece rook = copy[fromRank][rookFile];
             copy[fromRank][fromFile] = new NonePiece();
